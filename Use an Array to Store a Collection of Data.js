01211bdd1dec12a9810e26620a751f719d6f662a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-06-15 20:55:12 -0400

    Use an Array to Store a Collection of Data

---
+++ Use an Array to Store a Collection of Data.js
@@ -0,0 +1,11 @@
+/*
+Instructions
+
+-yourArray is an array
+-yourArray is at least 5 elements long
+-yourArray contains at least one boolean
+-yourArray contains at least one number
+-yourArray contains at least one string
+*/
+
+let yourArray =['Astra' ,'Rai', 36, true, false];
