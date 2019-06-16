5ee3ae2537122edcd5007913e1bf8c4514ab8bab
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-06-15 21:07:35 -0400

    Access an Array's Contents Using Bracket Notation

---
+++ Access an Array's Contents Using Bracket Notation.js
@@ -0,0 +1,15 @@
+/*
+Instructions
+-myArray[0] is equal to "a"
+-myArray[1] is no longer set to "b"
+-myArray[2] is equal to "c"
+-myArray[3] is equal to "d"
+*/
+
+
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+//use bracket notationto change element at 1st element of the array
+myArray[1] = 'i';
+//change code above this line
+console.log(myArray);//-> ["a", "i", "c", "d"]
