5f3b9eb34b958c287b9c76c52acb2b699ca67e9c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-12-28 10:15:46 -0500

    Testing objects for properties, use hasOwnProperty

---
+++ testing-objects-for-properties.js
@@ -0,0 +1,17 @@
+//freeCodeCamp
+//Basic JavaScript Exercises
+//Testing Objects for Properties
+
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+function checkObj(checkProp) {
+  if(myObj.hasOwnProperty(checkProp)){
+    return myObj[checkProp];
+    } else {
+    return "Not Found";
+  }
+}
+console.log(checkObj("gift"));
