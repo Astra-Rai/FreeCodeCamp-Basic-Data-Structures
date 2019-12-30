7d16be407481638f6c0c91226a226057acd69977
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-12-29 20:25:33 -0500

    Build _JavaScript_ Objects

---
+++ Build _JavaScript_ Objects.js
@@ -0,0 +1,19 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "rambo",
+  "legs": 5,
+  "tails": 2,
+  "friends": ["no new friends"] 
+};
+
+//access value of key in object myDog using dot notation
+console.log(myDog.friends);
