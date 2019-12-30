7dd09e1ec7e6df6af4cdba354cdffe0f652e3737
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-12-30 12:20:41 -0500

    Manipulating_Complex _Objects

---
+++ Manipulating_Complex _Objects.js
@@ -0,0 +1,31 @@
+/*freeCodeCamp | Basic JavaScript
+Manipulating Complex Objects
+Instructions: 
+-Add a new album to the myMusic array. 
+-Add artist and title strings, release_year number, 
+-and a formats array of strings.
+*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Adele",
+    "title": "All I Ask",
+    "release_year": 2016,
+    "formats": [
+      "YouTube Video",
+      "CD"
+    ]
+  }
+];
