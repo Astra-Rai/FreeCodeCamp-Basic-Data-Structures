501ca4eead989e4af0f4425c4996a5b1e814d0e8
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-12-31 14:54:52 -0500

    Iterate with JavaScript While Loops

---
+++ Iterate_with_JavaScript_ While_Loops.js
@@ -0,0 +1,19 @@
+//Basic JavaScript
+//Iterate with JavaScript While Loops
+
+// Setup
+//set myArray variable to an empty array
+
+var myArray = [];
+
+// Only change code below this line.
+var i =0;
+//while i less than 6....
+while(i < 6){
+    //push as element to the myArray
+    myArray.push(i);
+    //increment by one for each loop
+    i++;
+}
+console.log(myArray);
+//output-->[0,1,2,3,4,5]
