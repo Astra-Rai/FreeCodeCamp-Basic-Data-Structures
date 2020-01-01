a41c490c0af20bd5127c23be712346700a5b58b9
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-12-31 20:28:27 -0500

    Iterate with JavaScript For Loops

---
+++ Iterate with JavaScript For Loops.js
@@ -0,0 +1,13 @@
+/*
+-You should be using a for loop for this.
+Passed
+-myArray should equal [1,2,3,4,5].
+
+*/
+var myArray = [];
+
+for(var i = 1; i<6 ; i++){
+  myArray.push(i) 
+}
+
+console.log(myArray);
