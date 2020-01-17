56ff38a4efaffc06e49dd549deea5416c5a64368
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-01-17 15:50:13 -0500

    Switch Statements

---
+++ Selecting from Many Options with Switch Statements.js
@@ -0,0 +1,27 @@
+function caseInSwitch(val) {
+  var colorChoice= "";
+  // Only change code below this line
+ 
+  switch(val){
+ 
+    case 1:
+    answer = "red";
+    break;
+
+    case 2:
+    answer = "blue";
+    break;
+
+    case 3:
+    answer= "green";
+    break;
+    
+    case 4:
+    answer = "brown";
+    break;
+}
+  // Only change code above this line
+  return answer;
+}
+// Change this value to test
+console.log(caseInSwitch(3));
