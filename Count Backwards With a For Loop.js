c4b5a1460485270d7aa15b33569d0371b297640d
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-01-30 10:28:41 -0500

    Count Backwards With a For Loop

---
+++ Count Backwards With a For Loop.js
@@ -0,0 +1,11 @@
+//Count Backwards With a For Loop
+//create an empty array as a placeholder for odd numbers pushed to myArray
+var myArray = [];
+//starting point = 9, if i is greater than 10, decrement by 2, this will provide all odd numbers
+for(var i = 9; i > 0; i -=2){
+  //use push method and push odd numbers to myArray
+  myArray.push(i);
+}
+// Only change code below this line.
+console.log(myArray);
+
